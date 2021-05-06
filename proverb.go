ac14be217687bb02c7469f03f36b8652e308dc19
Rustam Khasanshin <rustery@gmail.com>
2021-05-06 20:27:56 +0300

    remove fmt module

--- proverb.go
+++ proverb.go
@@ -1,16 +1,14 @@
 // Package proverb is Exercism.io side exercise
 package proverb
 
-import "fmt"
-
 // Proverb — generate the relevant proverb, based on a list of inputs.
 func Proverb(rhyme []string) []string {
 	result := []string{}
 	if len(rhyme) > 0 {
 		for i := 0; i < len(rhyme)-1; i++ {
-			result = append(result, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
+			result = append(result, "For want of a "+rhyme[i]+" the "+rhyme[i+1]+" was lost.")
 		}
-		result = append(result, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+		result = append(result, "And all for the want of a "+rhyme[0]+".")
 	}
 	return result
 }
